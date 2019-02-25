e37dced9e00a2690f80867f82e5b473a8d563695
nirasan <niranirao@gmail.com>
2019-02-26 08:18:21 +0900

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,120 @@
+use std::io::{self, Read, Write};
+
 fn main() {
     println!("Hello, world!");
 }
 
 struct Processor {
     memory: Memory,
+    pointer: usize,
     tokens: Vec<Token>,
+}
+
+impl Processor {
+    fn new(tokens: Vec<Token>) -> Processor {
+        Processor{
+            memory: Memory::new(),
+            pointer: 0,
+            tokens
+        }
+    }
+
+    fn process(&mut self) {
+        use Token::*;
+        loop {
+            let result = self.tokens.get(self.pointer);
+            if let Some(token) = result {
+                match token {
+                    NEXT => self.next(),
+                    PREV => self.prev(),
+                    INCR => self.incr(),
+                    DECR => self.decr(),
+                    READ => self.read(),
+                    WRITE => self.write(),
+                    JUMP => self.jump(),
+                    BACK => self.back(),
+                }
+                self.pointer += 1;
+            } else {
+                break;
+            }
+        }
+    }
+
+    fn next(&mut self) {
+        self.memory.next()
+    }
+
+    fn prev(&mut self) {
+        self.memory.prev()
+    }
+
+    fn incr(&mut self) {
+        self.memory.incr()
+    }
+
+    fn decr(&mut self) {
+        self.memory.decr()
+    }
+
+    fn read(&mut self) {
+        let mut buf = [0];
+        io::stdin().read(&mut buf).ok().expect("read error");
+        self.memory.set(buf[0]);
+    }
+
+    fn write(&mut self) {
+        let buf = [self.memory.curr()];
+        io::stdout().write(&buf).ok().expect("write error");
+    }
+
+    fn jump(&mut self) {
+        if !self.memory.is_zero() {
+            return;
+        }
+        use Token::*;
+        let mut depth = 0;
+        for index in self.pointer..self.tokens.len() {
+            let ref token = self.tokens[index];
+            match token {
+                JUMP => depth += 1,
+                BACK => depth -= 1,
+                _ => ()
+            }
+            if depth < 0 {
+                self.pointer = index;
+                break;
+            }
+        }
+    }
+
+    fn back(&mut self) {
+        if self.memory.is_zero() {
+            return;
+        }
+        use Token::*;
+        let mut depth = 0;
+        for index in (0..self.pointer).rev() {
+            let ref token = self.tokens[index];
+            match token {
+                BACK => depth += 1,
+                JUMP => depth -= 1,
+                _ => ()
+            }
+            if depth < 0 {
+                self.pointer = index;
+                break;
+            }
+        }
+    }
+}
+
+#[test]
+fn processor_test() {
+    let code = ">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>++++++++[<++++>-]<+.[-]++++++++++.";
+    let tokens = Tokenizer::tokenize(code.to_string());
+    let mut processor = Processor::new(tokens);
+    processor.process();
 }
 
 struct Tokenizer {}
@@ -62,7 +172,7 @@ impl Memory {
     fn new() -> Memory {
         Memory{
             pointer: 0,
-            data: Vec::<u8>::new()
+            data: vec![0; 1000]
         }
     }
 
@@ -76,6 +186,14 @@ impl Memory {
 
     fn curr(&mut self) -> u8 {
         self.data[self.pointer]
+    }
+
+    fn is_zero(&mut self) -> bool {
+        self.data[self.pointer] == 0
+    }
+
+    fn set(&mut self, value: u8) {
+        self.data[self.pointer] = value
     }
 
     fn incr(&mut self) {
